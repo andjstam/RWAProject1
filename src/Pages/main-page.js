ea26e33b06762090ff1e032a890de6df2b3de7c0
andjstam <andjstam@elfak.rs>
2020-05-08 02:13:18 +0200

    main page 2

--- src/Pages/main-page.js
+++ src/Pages/main-page.js
@@ -3,16 +3,28 @@ import '../../css/main-page.css';
 export class MainPage{
     
     constructor(){
-        this.mainContainer=document.getElementById("main");
+        this.body=document.getElementById("main");
+    }
+
+    makeElement(el, name, parent)
+    {
+        let element=document.createElement(el);
+        element.className=name;
+        parent.appendChild(element);
+        return element;
     }
 
     drawMainView(){
     //dva diva(row), u jednom crta inicijalne stolove, u drugom pevaljku
+        const mainContainer=document.getElementById("main-content");
+        mainContainer.className="mainContainer";
 
-        const dobrodoslica=document.createElement("h2");
-        dobrodoslica.className="dobrdoslica";
-        dobrodoslica.innerHTML="Dobrodosli u kafanu Boemska!";
-        this.mainContainer.appendChild(dobrodoslica);
+        let gostiDiv=this.makeElement("div","gostiDiv",mainContainer);
+        gostiDiv.innerHTML="Ovde ce gosti";
+        let pevacicaDiv=this.makeElement("div","pevacicaDiv",mainContainer);
+        pevacicaDiv.innerHTML="Ovde muzika";
+
+        this.body.appendChild(mainContainer);
     }
 
     drawGuest(){
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: './dist'
   },
-  watch: true,
   devServer: {
       contentBase: './dist'
   },
